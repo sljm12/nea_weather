88d4a11f0d4f337aa39872c60e6567f4406338b9
sljm <sljm@mail.com>
2020-10-09 10:26:06 +0800

    added class for NeaWeather

--- nea_weather.py
+++ nea_weather.py
@@ -5,27 +5,32 @@ import geopandas as gpd
 from shapely.geometry import Point
 import os
 import sys
+from io import BytesIO
 
 
-def get_date_time(delta=0):
+def get_date_time(delta_hours=0, delta_minutes=0):
     """
 
-    :param delta: To move back the time by this value minutes. In case ur server and nea server time are not sync
+    :param delta_minutes: To move back the time by this value minutes. In case ur server and nea server time are not sync
     :return:
     """
     current = datetime.datetime.now()
-    minute = current.minute if current.minute%5 == 0 else current.minute -  (current.minute%5)
-    minute = minute - delta
-    return "%d%s%s%d%d0000" % (current.year, "%02d"% current.month, "%02d"%current.day, current.hour, minute)
+    minute = current.minute if current.minute%5 == 0 else current.minute - (current.minute%5)
+    minute = minute - delta_minutes if minute - delta_minutes > 0 else 0
+    return "%d%s%s%s%s0000" % (current.year, "%02d"% current.month, "%02d"%current.day, "%02d"%(current.hour+delta_hours), "%02d"%minute)
 
 
-def download_rain_file(dir):
-    filename = "dpsri_70km_%sdbr.dpsri.png" % get_date_time(5)
+def download_rain_file(mode="memory", dir="./"):
+    filename = "dpsri_70km_%sdbr.dpsri.png" % get_date_time(0, 5)
     url = "https://www.nea.gov.sg/docs/default-source/rain-area/%s" % (filename)
+    print(url)
     r = requests.get(url)
-    with open(os.path.join(dir, filename),'wb') as f:
-        f.write(r.content)
-    return os.path.join(dir, filename)
+    if mode == "memory":
+        return BytesIO(r.content)
+    else:
+        with open(os.path.join(dir, filename),'wb') as f:
+            f.write(r.content)
+        return os.path.join(dir, filename)
 
 
 def find_x_pixel(ul_x,lr_x, x_len, x):
@@ -50,14 +55,56 @@ def get_rain_area(im, x, y, upper_left_x, upper_left_y, lower_right_x, lower_rig
     return arr_points
 
 
+class NeaWeatherProcessing:
+    def __init__(self, rain_picture):
+        self.upper_left_x = 103.5544
+        self.lower_right_x = 104.1337
+        self.upper_left_y = 1.4771
+        self.lower_right_y = 1.1530
+        self.rain_df = self.generate_rain_data_frame(rain_picture)
+
+    def generate_rain_data_frame(self, picture_fp):
+        im = Image.open(picture_fp)
+        (x, y) = im.size
+        arr_points = self.get_rain_area(im, x, y, self.upper_left_x, self.upper_left_y, self.lower_right_x, self.lower_right_y)
+
+        return gpd.GeoDataFrame(data=arr_points, crs="EPSG:4326")
+
+    def get_rain_area(self, im, x, y, upper_left_x, upper_left_y, lower_right_x, lower_right_y):
+        arr_points = []
+        for px in range(x):
+            for py in range(y):
+                (r, g, b, a) = im.getpixel((px, py))
+                if a == 255:
+                    point = Point((find_x_pixel(upper_left_x, lower_right_x, x, px),
+                                   find_y_pixel(upper_left_y, lower_right_y, y, py)))
+                    arr_points.append({"type": "rain", "geometry": point})
+        return arr_points
+
+    def check_rain(self, long, lat, buffer_in_degrees):
+        newDf = self.create_location_df(long, lat, buffer_in_degrees)
+
+        intersect = gpd.overlay(self.rain_df, newDf, how="intersection")
+        return len(intersect)
+
+    def create_location_df(self, long, lat, buffer_in_degrees):
+        myLoc = Point((long, lat))
+        buf = myLoc.buffer(buffer_in_degrees)
+
+        with_buffer = []
+        with_buffer.append({"type": "location", "geometry": buf})
+        return gpd.GeoDataFrame(data=with_buffer, crs="EPSG:4326")
+
+
 if __name__ == "__main__":
     upper_left_x = 103.5544
     lower_right_x = 104.1337
     upper_left_y = 1.4771
     lower_right_y = 1.1530
-    location = (sys.argv[1], sys.argv[2])
+    location = (float(sys.argv[1]), float(sys.argv[2]))
 
-    filename = download_rain_file("rain")
+    filename = download_rain_file(mode="memory")
+    '''
     im = Image.open(filename)
     (x, y) = im.size
     arr_points = get_rain_area(im, x, y, upper_left_x, upper_left_y, lower_right_x, lower_right_y)
@@ -72,5 +119,8 @@ if __name__ == "__main__":
     newDf = gpd.GeoDataFrame(data=with_buffer, crs="EPSG:4326")
 
     intersect = gpd.overlay(rain_df, newDf, how="intersection")
+    '''
 
-    print(len(intersect))
+    a = NeaWeatherProcessing(filename)
+    intersect = a.check_rain(location[0], location[1], 0.009)
+    print(intersect)
